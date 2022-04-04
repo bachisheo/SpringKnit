541574383ca5ee4b9017e324c3dc5dbbe95e2f74
olga <bachisheo@gmail.com>
2022-04-04 20:50:43 +0700

    Добавлена страница просмотра подробностей о товаре

--- src/main/java/com/prompo/knit/controller/ProductController.java
+++ src/main/java/com/prompo/knit/controller/ProductController.java
@@ -13,18 +13,34 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
-    /** Список товаров */
+    /**
+     * Просмотр списка всех товаров в системе
+     * @param vars
+     * @return
+     */
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String index(Model vars) {
         /* Заполняем модель для представления */
         vars.addAttribute("products", productService.getAll());
-      //  vars.addAttribute("students", productService.getAllProducts());
         /* Возвращаем имя шаблона, который надо рендерить */
         return "index";
     }
+    @RequestMapping(method = RequestMethod.GET, value = "/e")
+    public String empty() {
+        /* Возвращаем имя шаблона, который надо рендерить */
+        return "empty_template";
+    }
 
+    /**
+     * Подробный просмотр информации о товаре
+     * @param productid id запрашиваемого товара
+     * @param vars
+     * @return
+     */
     @GetMapping("/product/{productid}")
-    private Product getProduct(@PathVariable("productid") Long productid){
-        return productService.find(productid).get();
+    private String getProduct(@PathVariable("productid") Long productid, Model vars){
+        //todo проверка на случай, если файл не найден
+        vars.addAttribute("product", productService.find(productid).get());
+        return "product_details";
     }
 }
