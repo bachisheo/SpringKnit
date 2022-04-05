49add390adc840b29f7cd5949825d3dec2fe6d3f
olga <bachisheo@gmail.com>
2022-04-05 16:42:50 +0700

    Реализован функционал редактирования полей продукта

--- src/main/java/com/prompo/knit/controller/ProductController.java
+++ src/main/java/com/prompo/knit/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.prompo.knit.Dao.ProductService;
 import com.prompo.knit.model.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -41,6 +42,37 @@ public class ProductController {
     private String getProduct(@PathVariable("productid") Long productid, Model vars){
         //todo проверка на случай, если файл не найден
         vars.addAttribute("product", productService.find(productid).get());
+        return "product_details";
+    }
+
+    /**
+     * Получение страницы редактирования продукта
+     * @param productid
+     * @param vars
+     * @return
+     */
+    @GetMapping("/edit/{productid}")
+    private String editProduct(@PathVariable("productid") Long productid, Model vars){
+        vars.addAttribute("product", productService.find(productid).get());
+        return "edit";
+    }
+
+    /**
+     * Сохранение результата изменения продукта
+     * @param product
+     * @param vars
+     * @param productid
+     * @return
+     */
+    @PostMapping("/edit/{productid}")
+    public String editProduct(@ModelAttribute Product product, Model vars, @PathVariable String productid) {
+        Product original = productService.find(Long.valueOf(productid)).get();
+        original.setName(product.getName());
+        original.setCount(product.getCount());
+        original.setDescription(product.getDescription());
+        original.setPrice(product.getPrice());
+        productService.update(original);
+        vars.addAttribute("product", original);
         return "product_details";
     }
 }
